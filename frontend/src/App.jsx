55b24650d4a1b95cf87b59b532306f7ab8f850e9
Devika Satheesan <devikasatheesan489@gmail.com>
2025-04-16 17:29:22 +0530

    addedbg

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+import CongratulationsCard from './Congratscard'
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  
+
+  return (
+ <CongratulationsCard/>
+  )
+}
+
+export default App
